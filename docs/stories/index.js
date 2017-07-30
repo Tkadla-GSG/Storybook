cfaebddf517156d5c2c18af267c196fe53962eee
Jakub Tkadlecek <jakub@mewssystems.com>
2017-07-30 11:59:45 +0200

    improved gitignore

--- docs/stories/index.js
+++ docs/stories/index.js
@@ -4,8 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button } from 'mews-ui';
+import { Button, FontIcon } from 'mews-ui';
 
 storiesOf('Button', module)
-  .addWithInfo('with text', '', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .addWithInfo('with some emoji', '', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .addWithInfo('default', '', () => <Button onClick={action('clicked')}>Button label</Button>)
+  .addWithInfo('raised primary', '', () => <Button onClick={action('clicked')} primary raised>Button label</Button>)
+  .addWithInfo('raised with icon', '', () => <Button icon={<FontIcon value={'star'} />} onClick={action('clicked')} raised>Button with icon</Button>);
